09b4dadd5ca0f75d4f4680d138a59b6820abdf91
Christina Will <cmkupiec@me.com>
2022-07-28 16:20:39 -0400

    changes code for local

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
